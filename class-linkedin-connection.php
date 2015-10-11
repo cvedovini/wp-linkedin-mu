0a05924225c8cb0437a823ac2dd3823d888c45be
Claude Vedovini <592099+cvedovini@users.noreply.github.com>
2015-10-11 05:43:42 +0000

    cleaning up repo

--- class-linkedin-connection.php
+++ class-linkedin-connection.php
@@ -8,109 +8,27 @@ class WPLinkedInMUConnection extends WPLinkedInConnection {
 		$this->user_id = $user_id;
 	}
 
-	protected function _set_cache($key, $value) {
-		return update_user_meta($this->user_id, $key, $value);
+	protected function set_cache($key, $value, $expires=0) {
+		return update_user_option($this->user_id, $key, $value);
 	}
 
-	protected function _get_cache($key) {
-		return get_user_meta($this->user_id, $key, true);
+	protected function get_cache($key, $default=false) {
+		$value = get_user_option($key, $this->user_id);
+		return ($value !== false) ? $value : $default;
 	}
 
-	protected function _delete_cache($key) {
-		global $wpdb;
-		$options = $wpdb->get_results("SELECT meta_key FROM {$wpdb->usermeta} WHERE user_id = '{$this->user_id}' AND meta_key LIKE '$key'" );
-
-		if (empty($options)) {
-			return false;
-		} else {
-			foreach ($options as $option) {
-				delete_user_meta($this->user_id, $option->meta_key);
-			}
-
-			return true;
-		}
+	protected function delete_cache($key) {
+		return delete_user_option($this->user_id, $key);
 	}
 
-	public function process_authorization($code, $state, $redirect_uri=false) {
-		if (get_option('wp-linkedin-mu_connect_with_linkedin')) {
-			if (isset($_REQUEST['error'])) {
-				$error = $_REQUEST['error'];
-				$error_description = $_REQUEST['error_description'];
-				$this->redirect($redirect_uri, 'error', "$error_description ($error)");
-			} elseif ($this->check_state_token($state)) {
-				$retcode = $this->retrieve_access_token($code, $redirect_uri);
-
-				if (!is_wp_error($retcode)) {
-					$this->access_token = $retcode->access_token;
-					$fields = array('id', 'email-address');
-					$fields = apply_filters('linkedin_connect_fields', $fields);
-					$profile = $this->get_profile(implode(',', $fields));
-
-					if (!$profile) {
-						$this->redirect($redirect_uri, 'error', __('Cannot fetch profile', 'wp-linkedin-mu'));
-						return;
-					}
-
-					$user_id = $this->get_or_create_user($profile);
-					if ($user_id) {
-						$this->user_id = $user_id;
-						wp_logout();
-						wp_set_auth_cookie($this->user_id);
-						wp_set_current_user($this->user_id);
-						$this->set_cache('oauthtoken', $retcode->access_token, $retcode->expires_in);
-						do_action('linkedin_user_connected', $profile);
-						$this->redirect($redirect_uri, 'success', __('Profile successfully updated', 'wp-linkedin-mu'));
-					} else {
-						$this->redirect($redirect_uri, 'error', __('Cannot find or create user', 'wp-linkedin-mu'));
-					}
-				} else {
-					$this->redirect($redirect_uri, 'error', $retcode->get_error_message());
-				}
-			} else {
-				$this->redirect($redirect_uri, 'error', __('Invalid state', 'wp-linkedin'));
-			}
-		} else {
-			parent::process_authorization($code, $state, $redirect_uri);
-		}
+	public function get_token_process_url() {
+		return site_url('/wp-admin/profile.php');
 	}
 
-	private function get_or_create_user($profile) {
-		if (is_user_logged_in()) {
-			$user_id = get_current_user_id();
-		    return $user_id;
-		}
+	protected function send_invalid_token_email() {
+		$send_mail = get_user_option('wp-linkedin_sendmail_on_token_expiry');
 
-		$user_by_id = get_users(array('meta_key' => 'wp-linkedin-mu_profile_id',
-						'meta_value' => $profile->id) );
-
-		if (count($user_by_id) > 0) {
-			update_user_meta($user_by_id[0]->ID, 'wp-linkedin-mu_profile_id', $profile->id);
-		    return $user_by_id[0]->ID;
-		}
-
-		$email = $profile->emailAddress;
-
-		if (email_exists($email)) {
-		    $user = get_user_by('email', $email);
-		    update_user_meta($user->ID, 'wp-linkedin-mu_profile_id', $profile->id);
-		    return $user->ID;
-		}
-
-		$user_info = array(
-				'user_login'	=> $email,
-				'user_email'	=> $email,
-				'user_pass'		=> wp_generate_password(15),
-				'role' 			=> get_option('wp-linkedin-mu_default_user_role', 'subscriber'));
-
-	    $user_id = wp_insert_user($user_info);
-	    update_user_meta($user_id, 'wp-linkedin-mu_profile_id', $profile->id);
-	    return $user_id;
-	}
-
-	public function send_invalid_token_email() {
-		$send_mail = get_user_meta($this->user_id, 'wp-linkedin_sendmail_on_token_expiry');
-
-		if ($send_mail && !$this->get_cache('invalid_token_mail_sent')) {
+		if ($send_mail && !$this->get_cache('wp-linkedin_invalid_token_mail_sent')) {
 			$user_info = get_userdata($this->user_id);
 			$blog_name = get_option('blogname');
 			$user_email = $user_info->user_email;
@@ -123,7 +41,7 @@ class WPLinkedInMUConnection extends WPLinkedInConnection {
 			$message .= "\n" . __('-Thank you.', 'wp-linkedin-mu');
 
 			$sent = wp_mail($user_email, $subject, $message, $header);
-			$this->update_user_meta('wp-linkedin_invalid_token_mail_sent', $sent);
+			$this->set_cache('wp-linkedin_invalid_token_mail_sent', $sent);
 		}
 	}
 
---
+++ updater.php
@@ -0,0 +1,129 @@
+<?php
+define('WP_LINKEDIN_MU_STORE_URL', 'http://vedovini.net' );
+define('WP_LINKEDIN_MU_ITEM_NAME', 'WP LinkedIn Multi-Users' );
+define('WP_LINKEDIN_MU_AUTHOR', 'Claude Vedovini');
+
+
+if (!class_exists('EDD_SL_Plugin_Updater')) {
+	include( dirname( __FILE__ ) . '/lib/EDD_SL_Plugin_Updater.php' );
+}
+
+
+function wp_linkedin_mu_plugin_updater() {
+	// retrieve our license key from the DB
+	$license_key = trim(get_option('wp-linkedin-mu_license_key'));
+
+	// setup the updater
+	$edd_updater = new EDD_SL_Plugin_Updater(WP_LINKEDIN_MU_STORE_URL, __FILE__, array(
+		'version' 	=> '1.0', 				// current version number
+		'license' 	=> $license_key, 		// license key (used get_option above to retrieve from DB)
+		'item_name' => WP_LINKEDIN_MU_ITEM_NAME, 	// name of this plugin
+		'author' 	=> WP_LINKEDIN_MU_AUTHOR  // author of this plugin
+	));
+}
+add_action( 'admin_init', 'wp_linkedin_mu_plugin_updater' );
+
+
+function wp_linkedin_mu_license_menu() {
+	register_setting('wp-linkedin', 'wp-linkedin-mu_license_key', 'wp_linkedin_mu_sanitize_license');
+	add_settings_field('wp-linkedin-mu_license_key', __('Multi-users License Key', 'wp-linkedin-mu'),
+			'wp_linkedin_mu_license_key_field', 'wp-linkedin');
+}
+add_action('admin_menu', 'wp_linkedin_mu_license_menu', 30);
+
+
+function wp_linkedin_mu_license_key_field() {
+	$license = get_option('wp-linkedin-mu_license_key');
+	$status = get_option('wp-linkedin-mu_license_status'); ?>
+	<p><input id="wp-linkedin-mu_license_key" name="wp-linkedin-mu_license_key" type="text" class="regular-text" value="<?php esc_attr_e($license); ?>" />
+	<?php if ($status !== false && $status == 'valid') { ?>
+		<span style="color:green;"><?php _e('Active', 'wp-linkedin-mu'); ?></span>
+		<?php wp_nonce_field('wp_linkedin_mu_nonce', 'wp_linkedin_mu_nonce'); ?>
+		<input type="submit" class="button-secondary" name="wp_linkedin_mu_license_deactivate" value="<?php _e('Deactivate License', 'wp-linkedin-mu'); ?>"/>
+	<?php } else {
+		wp_nonce_field('wp_linkedin_mu_nonce', 'wp_linkedin_mu_nonce'); ?>
+		<input type="submit" class="button-secondary" name="wp_linkedin_mu_license_activate" value="<?php _e('Activate License', 'wp-linkedin-mu'); ?>"/>
+	<?php } ?></p><?php
+}
+
+
+function wp_linkedin_mu_sanitize_license($new) {
+	$old = get_option('wp-linkedin-mu_license_key');
+
+	if ($old && $old != $new) {
+		delete_option('wp-linkedin-mu_license_status'); // new license has been entered, so must reactivate
+	}
+
+	return $new;
+}
+
+
+function wp_linkedin_mu_activate_license() {
+	// listen for our activate button to be clicked
+	if (isset( $_POST['wp_linkedin_mu_license_activate'])) {
+		// run a quick security check
+	 	if (!check_admin_referer('wp_linkedin_mu_nonce', 'wp_linkedin_mu_nonce'))
+			return; // get out if we didn't click the Activate button
+
+		// retrieve the license from the database
+		$license = trim(get_option('wp-linkedin-mu_license_key'));
+
+		// data to send in our API request
+		$api_params = array(
+			'edd_action'=> 'activate_license',
+			'license' 	=> $license,
+			'item_name' => urlencode(WP_LINKEDIN_MU_ITEM_NAME)
+		);
+
+		// Call the custom API.
+		$response = wp_remote_get(add_query_arg($api_params, WP_LINKEDIN_MU_STORE_URL), array('timeout' => 15, 'sslverify' => false));
+
+		// make sure the response came back okay
+		if (is_wp_error($response))
+			return false;
+
+		// decode the license data
+		$license_data = json_decode(wp_remote_retrieve_body($response));
+
+		// $license_data->license will be either "active" or "inactive"
+		update_option('wp-linkedin-mu_license_status', $license_data->license);
+
+	}
+}
+add_action('admin_init', 'wp_linkedin_mu_activate_license');
+
+
+function wp_linkedin_mu_deactivate_license() {
+	// listen for our activate button to be clicked
+	if (isset($_POST['wp_linkedin_mu_license_deactivate'])) {
+		// run a quick security check
+	 	if(!check_admin_referer('wp_linkedin_mu_nonce', 'wp_linkedin_mu_nonce'))
+			return; // get out if we didn't click the Activate button
+
+		// retrieve the license from the database
+		$license = trim(get_option('wp-linkedin-mu_license_key'));
+
+		// data to send in our API request
+		$api_params = array(
+			'edd_action'=> 'deactivate_license',
+			'license' 	=> $license,
+			'item_name' => urlencode(WP_LINKEDIN_MU_ITEM_NAME)
+		);
+
+		// Call the custom API.
+		$response = wp_remote_get(add_query_arg($api_params, WP_LINKEDIN_MU_STORE_URL), array('timeout' => 15, 'sslverify' => false));
+
+		// make sure the response came back okay
+		if (is_wp_error($response))
+			return false;
+
+		// decode the license data
+		$license_data = json_decode(wp_remote_retrieve_body($response));
+
+		// $license_data->license will be either "deactivated" or "failed"
+		if ($license_data->license == 'deactivated')
+			delete_option('wp-linkedin-mu_license_status');
+
+	}
+}
+add_action('admin_init', 'wp_linkedin_mu_deactivate_license');
